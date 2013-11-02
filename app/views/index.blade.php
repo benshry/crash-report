cc09dc3c140645db596bd8656abfd31c8d4516c4
Ben Shryock <benshry@gmail.com>
2013-11-02 16:04:45 -0400

    bootstrap and beginning login

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -2,14 +2,24 @@
 
 <html>
     <head>
+        <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css">
         {{ HTML::script('/js/jquery-1.10.2.min.js') }}
+        {{ HTML::script('/js/bootstrap.min.js') }}
         <title>Crashr</title>
     </head>
     <body>
-        <div id="left">
-            Hi Rob!!!
+        <div id="top">
         </div>
-        <div id="right">
+        <div id="middle">
+            <form>
+                <fieldset>
+                    <legend>Log in.</legend>
+                    <label>Username</label>
+                    <input type="text" placeholder="Username">
+                    <input type="password" placeholder="Password">
+                    <button type="submit" class="btn btn-large">Log In</button>
+                </fieldset>
+            </form>
         </div>
     <body>
 </html>
